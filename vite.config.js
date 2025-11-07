025bea4d70c0bbe0fb138f67ce75c879618b3fd9
KOWSHICK-KUMAR <kowshickkumarg@gmail.com>
2025-11-07 14:50:10 +0530

    Set base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   // Set base for GitHub Pages: https://<user>.github.io/<repo>/
+  base: '/https://kowshick-kumar.github.io/RentSecure//',
+})
 })
 
